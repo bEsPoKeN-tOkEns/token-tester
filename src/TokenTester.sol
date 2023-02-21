d31d8580064f9e264dfce76188aae41b68afbc6d
saucepoint <saucepoint@protonmail.com>
2023-02-21 14:49:37 -0700

    working example

--- src/TokenTester.sol
+++ src/TokenTester.sol
@@ -1,22 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-
+import {WETH} from "solmate/tokens/WETH.sol";
 import {MockERC20} from "./tokens/MockERC20.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
-contract TokenTester is Test {
-    // IERC20 public token;
+contract TokenTester {
+    IERC20 public testToken;
 
     address[] public tokens;
 
     constructor() {
         tokens.push(address(new MockERC20("Test", "TST", 18)));
+        tokens.push(address(new WETH()));
     }
     
     modifier usesTokenTester() {
         uint256 i;
         for (i; i < tokens.length;) {
+            testToken = IERC20(tokens[i]);
             _;
             unchecked {
                 ++i;
--- test/Example.t.sol
+++ test/Example.t.sol
@@ -4,11 +4,6 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/Counter.sol";
 import {TokenTester} from "../src/TokenTester.sol";
-//import {IERC20} from "solmate/tokens/ERC20.sol";
-
-interface IERC20 {
-    function balanceOf(address account) external view returns (uint256);
-}
 
 contract ExampleTest is Test, TokenTester {
     Counter public counter;
@@ -18,8 +13,7 @@ contract ExampleTest is Test, TokenTester {
         counter.setNumber(0);
     }
 
-    function testDeposit(address token) public usesToken(token) {
-        IERC20 testToken = IERC20(token);
+    function testDeposit() public usesTokenTester {
         assertEq(testToken.balanceOf(address(this)), 0);
     }
 
